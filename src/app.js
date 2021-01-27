89b2cdba8e65c7a3163f3feb5b65aeb3efc3b34a
keithfilegar <keith.filegar@gmail.com>
2021-01-26 19:33:58 -0800

    add get and post endpoints for list and card, delete for list

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const winston = require('winston')
 const { NODE_ENV } = require('./config')
+const { json } = require('express')
+const { v4: uuid} = require('uuid')
 
 const app = express()
 
@@ -14,9 +17,187 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json())
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'info.log' })
+    ]
+});
+
+if(NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
+const cards = [{
+    id: 1,
+    title: 'Task One',
+    content: 'This is card one'
+}];
+const lists = [{
+    id: 1,
+    header: 'List one',
+    cardIds: [1]
+}]
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    console.log(apiToken)
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`)
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+})
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
+})
+
+app.get('/card', (req, res) => {
+    res
+        .json(cards);
+})
+
+app.get('/card/:id', (req, res) => {
+    const { id } = req.params;
+    const card = cards.find(c => c.id == id);
+
+    if(!card) {
+        logger.error(`Card with id ${id} not found.`);
+        return res
+            .status(404)
+            .send('Card Not Found')
+    }
+
+    res.json(card)
+})
+
+app.post('/card', (req, res) => {
+    const { title, content } = req.body;
+
+    if(!title) {
+        logger.error(`Title is required`);
+        return res
+            .status(400)
+            .send('Invalid data')
+    }
+
+    if(!content) {
+        logger.error(`Content is required`);
+        return res
+            .status(400)
+            .send('Invalid data')
+    }
+
+    const id = uuid();
+
+    const card = {
+        id,
+        title,
+        content
+    };
+
+    cards.push(card);
+
+    logger.info(`Card with id ${id} created`)
+
+    res
+        .status(201)
+        .location(`http://localhost:8000/card/${id}`)
+        .json(card)
+})
+
+app.get('/list', (req, res) => {
+        res
+            .json(lists);
+})
+
+app.get('/list/:id', (req, res) => {
+    const { id } = req.params;
+    const list = lists.find(li => li.id == id)
+
+    if(!list) {
+        logger.error(`List with id ${id} not found.`)
+        return res
+            .status(404)
+            .send('List Not Found')
+    }
+
+    res.json(list)
+})
+
+app.post('/list', (req, res) => {
+    const { header, cardIds = [] } = req.body;
+
+    if(!header) {
+        logger.error(`Header is required`);
+        return res
+            .status(400)
+            .send('Invalid data')
+    }
+
+    //check cardIds
+    if(cardIds.length > 0) {
+        let valid = true;
+        cardIds.forEach(cid => {
+            const card = cards.find(c => c.id == cid);
+            if(!card) {
+                logger.error(`Card with id ${cid} not found in card array.`);
+                valid = false;
+            }
+        });
+
+        if(!valid) {
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
+    }
+
+    const id = uuid();
+    const list = {
+        id,
+        header,
+        cardIds
+    };
+
+    lists.push(list);
+
+    logger.info(`List with id ${id} created`)
+
+    res
+        .status(201)
+        .location(`http://localhost:8000/list/${id}`)
+        .json({id});
+
+})
+
+app.delete('/list/:id', (req, res) => {
+    const { id } = req.params;
+    
+    const listIndex = lists.findIndex(li => li.id == id);
+
+    if (listIndex === -1) {
+        logger.error(`List with ${id} not found.`);
+        return res
+            .status(404)
+            .send('Not Found')
+    }
+
+    lists.splice(listIndex, 1);
+
+    logger.info(`List with id ${id} deleted.`);
+    res
+        .status(204)
+        .send('Not Found')
 })
 
 app.use(function errorHandler(error, req, res, next) {
